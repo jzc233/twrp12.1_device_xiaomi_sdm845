25dcbb771bea9ac4cf2b7bb562bbe86950572ff3
jzc233 <63482703+jzc233@users.noreply.github.com>
2025-03-13 20:54:55 +0800

    Update twrp_equuleus.mk

--- twrp_equuleus.mk
+++ twrp_equuleus.mk
@@ -19,14 +19,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from device
-$(call inherit-product, device/xiaomi/equuleus/device.mk)
+$(call inherit-product, device/xiaomi/ursa/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := twrp_equuleus
-PRODUCT_DEVICE := equuleus
+PRODUCT_NAME := twrp_ursa
+PRODUCT_DEVICE := ursa
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := MI 8 UD
+PRODUCT_MODEL := MI 8 Explorer Edition
 PRODUCT_MANUFACTURER := Xiaomi
